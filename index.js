4d594e6c2506a3bd11e79033da20425d6bab00c4
fand <fand@gmork.in>
2015-08-18 12:08:14 +0900

    support all EventEmitter public methods

--- index.js
+++ index.js
@@ -2,35 +2,41 @@
 
 import { EventEmitter } from 'events';
 
+const methods = [
+  'addListener',
+  'emit',
+  'listeners',
+  'on',
+  'once',
+  'removeAllListeners',
+  'removeListener',
+
+  'setMaxListeners',
+  'getMaxListeners',
+];
+
+// Get or create eventemitter for current instance
 const emitters = new WeakMap();
+const getEmitter = function (obj) {
+  let emitter = emitters.get(obj);
+  if (!emitter) {
+    emitter = new EventEmitter();
+    emitters.set(obj, emitter);
+  }
+  return emitter;
+};
 
 export default function EventEmitterDecorator (klass) {
 
-  if (klass.prototype.on) {
-    throw new Error('"on" method is already defined!');
-  }
-  if (klass.prototype.off) {
-    throw new Error('"off" method is already defined!');
-  }
-
-  // Get or create eventemitter for current instance
-  const getEmitter = function (obj) {
-    let emitter = emitters.get(obj);
-    if (!emitter) {
-      emitter = new EventEmitter();
-      emitters.set(obj, emitter);
+  methods.forEach((method) => {
+    if (klass.prototype[method]) {
+      throw new Error(`"${method}" method is already defined!`);
     }
-    return emitter;
-  };
 
-  klass.prototype.on = function () {
-    const emitter = getEmitter(this);
-    emitter.on.apply(this, arguments);
-  };
-
-  klass.prototype.emit = function () {
-    const emitter = getEmitter(this);
-    emitter.emit.apply(this, arguments);
-  };
+    klass.prototype[method] = function () {
+      const emitter = getEmitter(this);
+      emitter[method].apply(this, arguments);
+    };
+  });
 
 }
