b376c8343784db3e8824faac9fa61d5b9f6c3050
fand <fand@gmork.in>
2015-08-18 18:10:54 +0900

    return results of EventEmitter methods correctly

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ export default function EventEmitterDecorator (klass) {
 
     klass.prototype[method] = function () {
       const emitter = getEmitter(this);
-      emitter[method].apply(emitter, arguments);
+      return emitter[method].apply(emitter, arguments);
     };
   });
 
