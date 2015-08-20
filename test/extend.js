704c3588d9eb03dfc65919d9a6d081e187296c40
fand <fand@gmork.in>
2015-08-20 10:41:43 +0900

    test all methods for EventEmitter cant be overridden

--- test/extend.js
+++ test/extend.js
@@ -1,17 +1,35 @@
 'use strict';
 
-import EventEmitter from '../index';
+import EventEmitter from 'events';
+import EventEmitterDecorator from '../index';
 import assert from 'power-assert';
 import sinon from 'sinon';
 
 describe('Class', function() {
 
   it('throws errors when the class already has methods named same as EventEmitter methods', function () {
-    assert.throws(function () {
-        @EventEmitter
-        class Foo {
-          on () {}
-        }
-    }, 'on method is already defined!');
+    const methods = [
+      'addListener',
+      'emit',
+      'listeners',
+      'on',
+      'once',
+      'removeAllListeners',
+      'removeListener',
+
+      'setMaxListeners',
+      'getMaxListeners',
+    ];
+
+    methods.forEach((method) => {
+      if (! EventEmitter.prototype[method]) { return; }
+      assert.throws(function () {
+          @EventEmitterDecorator
+          class Foo {
+            [method] () {}
+          }
+      }, `"${method}" method is already defined!`);
+    });
   });
+
 });
