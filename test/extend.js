8b9699f5d63cfa02b55f48f8cff30aa2a29ab284
fand <fand@gmork.in>
2015-08-20 10:44:42 +0900

    add tests for a class which extends a class with conflicting methods

--- test/extend.js
+++ test/extend.js
@@ -5,21 +5,22 @@ import EventEmitterDecorator from '../index';
 import assert from 'power-assert';
 import sinon from 'sinon';
 
+const methods = [
+  'addListener',
+  'emit',
+  'listeners',
+  'on',
+  'once',
+  'removeAllListeners',
+  'removeListener',
+
+  'setMaxListeners',
+  'getMaxListeners',
+];
+
 describe('Class', function() {
 
   it('throws errors when the class already has methods named same as EventEmitter methods', function () {
-    const methods = [
-      'addListener',
-      'emit',
-      'listeners',
-      'on',
-      'once',
-      'removeAllListeners',
-      'removeListener',
-
-      'setMaxListeners',
-      'getMaxListeners',
-    ];
 
     methods.forEach((method) => {
       if (! EventEmitter.prototype[method]) { return; }
@@ -30,6 +31,29 @@ describe('Class', function() {
           }
       }, `"${method}" method is already defined!`);
     });
+
+  });
+
+});
+
+describe('SubClass', function() {
+
+  it('throws errors when the base class of the class already has methods named same as EventEmitter methods', function () {
+
+    methods.forEach((method) => {
+      if (! EventEmitter.prototype[method]) { return; }
+      assert.throws(function () {
+
+          class Bar {
+            [method] () {}
+          }
+
+          @EventEmitterDecorator
+          class Foo extends Bar {}
+
+      }, `"${method}" method is already defined!`);
+    });
+
   });
 
 });
